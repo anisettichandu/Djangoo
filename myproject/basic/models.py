635d062dce12ebffe19c61e18403be81deb76d16
anisettichandu <anisettichandu9398@gmail.comgit config --listgit config --global user.email anisettichandu9398@gmail.com>
2025-11-22 11:06:49 +0530

    commit

--- myproject/basic/models.py
+++ myproject/basic/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 class Users(models.Model):
-    username=models.CharField(max_length=100,unique=True)
+    username=models.CharField(max_length=100)
     email=models.EmailField(max_length=254)
     password=models.CharField(max_length=100)
